e01866666ede91ca334c1d1753a3c58476375a94
Arnold Siyabonga Mdluli <arnoldmdluli00@gmail.com>
2022-12-07 00:04:42 +0000

    updated the base_models file

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,23 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from os import getenv
 
+STORAGE = getenv("HBNB_TYPE_STORAGE")
+
+if STORAGE == "db":
+    Base = declarative_base()
+else:
+    Base = object
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), unique=True, nullable=False, primary_key=True)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, nullable=False, default=datetime=utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
