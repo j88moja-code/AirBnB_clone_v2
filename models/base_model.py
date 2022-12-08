3c7abcc57c1735645db1bab6cdb03f3c25525f9a
Arnold Siyabonga Mdluli <arnoldmdluli00@gmail.com>
2022-12-08 02:30:58 +0000

    Task 6

--- models/base_model.py
+++ models/base_model.py
@@ -26,8 +26,9 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
+            if "updated_at" in kwargs and "updated_at" in kwargs\
+                  and "__class__" in kwargs:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
@@ -45,6 +46,7 @@ class BaseModel:
         from models import storage
         self.updated_at = datetime.now()
         storage.save()
+        storage.new(self)
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -54,4 +56,10 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        dictionary.pop("_sa_instance_state", None)
         return dictionary
+
+    def delete(self):
+        """ method to delete the current instance from storage """
+        from models import storage
+        storage.delete(self)
