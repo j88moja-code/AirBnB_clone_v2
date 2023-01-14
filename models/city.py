50abd60858378120dca32fe60c22e0ce3bef7b34
j88moja-code <sitholent@gmail.com>
2023-01-14 10:50:44 +0200

    Update logic

--- models/city.py
+++ models/city.py
@@ -1,30 +1,22 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from sqlalchemy import Column, Integer, String, ForeignKey
+"""Defines the City class."""
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
-from models.city import City
-from os import getenv
-
-STORAGE = getenv("HBNB_TYPE_STORAGE")
 
 
-class State(BaseModel, Base):
-    """ State class for the states of a country """
-    __tablename__ = 'states'
-    if STORAGE == "db":
-        name = Column(String(128), nullable=False)
-        cities = relationship(
-            'City', backref='states', cascade="all, delete-orphan")
-    else:
-        name = ""
-
-        @property
-        def cities(self):
-            from models import storage
-            list_city = []
-            all_ins = storage.all(City)
-            for value in all_ins.values():
-                if value.state_id == self.id:
-                    list_city.append(value)
-            return list_city
+class City(BaseModel, Base):
+    """Represents a city for a MySQL database.
+    Inherits from SQLAlchemy Base and links to the MySQL table cities.
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store Cities.
+        name (sqlalchemy String): The name of the City.
+        state_id (sqlalchemy String): The state id of the City.
+    """
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities", cascade="delete")
